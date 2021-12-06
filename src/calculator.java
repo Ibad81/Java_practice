9e5989521a21e015ad0d654812b2c737be1e2cf4
ibad81 <ibadbaig921@gmail.com>
2021-12-06 05:15:15 -0800

    Practice Question

---
+++ src/calculator.java
@@ -0,0 +1,15 @@
+//public class calculator {
+//    int num1,num2;
+//    static double powerInt(int num1, int num2){
+//        return Math.pow(num1,num2);
+//
+//    }
+//    static double powerDouble(double num1, int num2){
+//        return Math.pow(num1,num2);
+//    }
+//}
+//class Ques{
+//    public static void main(String[] args) {
+//        calculator ca=new
+//    }
+//}
