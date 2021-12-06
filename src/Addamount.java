42124fccbc76fa9e9af970735480419662025db4
ibad81 <ibadbaig921@gmail.com>
2021-12-06 05:20:33 -0800

    Practice Question

--- src/Addamount.java
+++ src/Addamount.java
@@ -1,21 +1,21 @@
-//public class Addamount {
-//    int Amount=50;
-//    public Addamount(){
-//
-//    }
-//    public Addamount(int a){
-//        Amount=a+Amount;
-//    }
-//
-//    public void displayAmount(){
-//        System.out.println(Amount);
-//    }
-//}
-//class ans{
-//    public static void main(String[] args) {
-//        Addamount A= new Addamount();
-//        Addamount B = new Addamount(40);
-//        A.displayAmount();
-//        B.displayAmount();
-//    }
-//}
+public class Addamount {
+    int Amount=50;
+    public Addamount(){
+
+    }
+    public Addamount(int a){
+        Amount=a+Amount;
+    }
+
+    public void displayAmount(){
+        System.out.println(Amount);
+    }
+}
+class ans{
+    public static void main(String[] args) {
+        Addamount A= new Addamount();
+        Addamount B = new Addamount(40);
+        A.displayAmount();
+        B.displayAmount();
+    }
+}
