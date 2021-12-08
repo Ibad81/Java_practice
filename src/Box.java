0cf8aace03af10128f32561d1d3ba3ba24d5f902
ibad81 <ibadbaig921@gmail.com>
2021-12-08 09:57:33 -0800

    Practice Question

--- src/Box.java
+++ src/Box.java
@@ -1,19 +1,22 @@
-public class Box {
-    int width,height,depth,vol;
-    Box(int w,int h,int d){
-        width=w;
-        height=h;
-        depth=d;
-    }
-    void result(){
-        vol=(width*height*depth);
-        System.out.println(vol);
-    }
-}
-
-class Test{
-    public static void main(String[] args) {
-        Box obj=new Box(2,3,1);
-        obj.result();
-    }
-}
+//public class Box {
+//    int width,height,depth,vol;
+//    Box(int w,int h,int d){
+//        width=w;
+//        height=h;
+//        depth=d;
+//    }
+//    void result(){
+//        vol=(width*height*depth);
+//        System.out.println(vol);
+//    }
+//}
+//
+//class Test{
+//    public static void main(String[] args) {
+//        Box obj=new Box(2,3,1);
+//        obj.result();
+//    }
+//}
+//
+//
+//
