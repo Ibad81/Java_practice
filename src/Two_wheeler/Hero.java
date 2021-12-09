9a41495a0834ad54cd529266dfffcbc1b1c1fff2
ibad81 <ibadbaig921@gmail.com>
2021-12-09 12:45:21 +0530

    Package Question

---
+++ src/Two_wheeler/Hero.java
@@ -0,0 +1,21 @@
+package Two_wheeler;
+
+import automobile.Vechile;
+
+public class Hero extends Vechile {
+
+    @Override
+    public String getModelName() {
+        return null;
+    }
+
+    @Override
+    public String getregistration() {
+        return null;
+    }
+
+    @Override
+    public String getOwnerName() {
+        return null;
+    }
+}
